66a927bec54d254cad079d80c496027420825d17
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-08-22 15:29:56 -0300

    Update files

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ function fetchPokemon(pokeId) {
 	const url = `https://pokeapi.co/api/v2/pokemon/${pokeId}`;
 	fetch(url).then((res) => {
 		return res.json();
+
 	})
 	.then((data) => {
+		console.log(data);
 		const pokemon = {
 			name: data.name,
 			id: data.id,
 			image: data.sprites['front_default'],
-			type: data.types.map((type) => type.type.name).join(', ')
+			info: data.height + ", " + data.weight,
+			abilities: data.abilities.map((abilities) => abilities.ability.name).join(', '),
+			type: data.types.map((type) => type.type.name).join(', '),
+			stats: data.stats.map((stats) => stats.stat.name + ": " + stats.base_stat).join(', ')
+			//moves: data.moves.map((moves) => moves.move.name).join(', ')
 		};
-			displayPokemon(pokemon);
+		displayPokemon(pokemon);
+	}).catch((data) => {
+		alert("this pokemon does not exists");
 	});
 };
-
 
 function displayPokemon(pokemon) {
 	const pokemonCard = `
 		<li>
 			<img src="${pokemon.image}"/>
 			<h2>${pokemon.id}. ${pokemon.name}</h2>
+			<p>${pokemon.info}</p>
 			<p>${pokemon.type}</p>
+			<p>${pokemon.abilities}</p>
+			<p>${pokemon.stats}</p>
 		</li>
 		`;
 	pokedex.innerHTML = pokemonCard;
@@ -30,7 +40,8 @@ function displayPokemon(pokemon) {
 
 document.querySelector("button").addEventListener("click", () => {
 	const inputValue = document.querySelector("#pokemoninput").value;
-	const getId = inputValue;
+	const lowerValue = inputValue.toLowerCase();
+	const getId = lowerValue;
 
 	fetchPokemon(getId);
 })
